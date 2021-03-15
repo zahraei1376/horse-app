d0ee412c628dd5dc6080a40c5c07aceec7d103e8
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-03-15 04:16:22 +0330

    instantiate jwt lib

--- backend/src/extensions.py
+++ backend/src/extensions.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
 from src.main import app
@@ -6,3 +7,4 @@ from src.main import app
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt_manager = JWTManager(app)
