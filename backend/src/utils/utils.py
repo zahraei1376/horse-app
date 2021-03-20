04f9b49ed792536c89db09943fb0b6b2363d16c2
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-03-20 20:05:00 +0330

    create has_role decorator

--- backend/src/utils/utils.py
+++ backend/src/utils/utils.py
@@ -1,6 +1,8 @@
 from functools import wraps
 
 from flask import request
+from flask_jwt_extended import get_jwt, verify_jwt_in_request
+
 
 def is_body_json(func):
     @wraps(func)
@@ -12,3 +14,16 @@ def is_body_json(func):
     return wrapper
 
 
+def has_role(role='USER'):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request()
+            current_role = get_jwt().get('role')
+            if current_role != role:
+                return {'errors': 'You haven\'t permit'}, 403
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
