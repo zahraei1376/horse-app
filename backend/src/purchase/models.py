3f8aa88da6eb1d79849f323892a875f778cc8ba9
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-03-20 08:59:32 +0330

    move Plan model into plan module

--- backend/src/purchase/models.py
+++ backend/src/purchase/models.py
@@ -1,23 +1,6 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, Text, Float, DateTime, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Text, Float, Boolean, ForeignKey
 
 from src.extensions import db
-
-
-class Plan(db.Model):
-    __tablename__ = 'plans'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String(255), nullable=False, unique=True)
-    description = Column(Text, nullable=False)
-    price = Column(Float(precision=2), nullable=False)
-    creation_datetime = Column(DateTime, default=datetime.now)
-    modified_datetime = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    horses = relationship('Horse', backref='plan')
-
-    def __repr__(self) -> str:
-        return self.title
 
 
 class Horse(db.Model):
